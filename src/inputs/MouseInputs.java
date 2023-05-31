12d6603fb4cc26b7d70500822096f29c05642458
sankaaaa <olexa2404@gmail.com>
2023-06-01 02:33:17 +0300

    Update MouseInputs.java

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -1,5 +1,6 @@
 package inputs;
 
+import gameStates.GameState;
 import main.GamePanel;
 
 import java.awt.event.MouseEvent;
@@ -13,10 +14,17 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
     }
     @Override
     public void mouseClicked(MouseEvent e) {
-        if(e.getButton() == MouseEvent.BUTTON1)
-            gamePanel.getGame().getPlayer().setAttacking(true);
+        switch (GameState.state){
+            case MENU:
+                gamePanel.getGame().getMenu().mouseClicked(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().mouseClicked(e);
+                break;
+            default:
+                break;
+        }
     }
-
     @Override
     public void mousePressed(MouseEvent e) {
 

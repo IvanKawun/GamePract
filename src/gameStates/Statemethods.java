52b40f42128fa2c571fcc14392424d053cec52f3
sankaaaa <olexa2404@gmail.com>
2023-06-01 18:31:20 +0300

    Update Statemethods.java

--- src/gameStates/Statemethods.java
+++ src/gameStates/Statemethods.java
@@ -1,9 +1,10 @@
 package gameStates;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
-public interface Statemethods {
+public interface StateMethods {
     public void update();
     public void draw(Graphics g);
     public void mouseClicked(MouseEvent e);
@@ -12,4 +13,6 @@ public interface Statemethods {
     public void mouseMoved(MouseEvent e);
     public void keyPressed(KeyEvent e);
     public void keyReleased(KeyEvent e);
+
+
 }

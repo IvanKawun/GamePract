23462e2d7fbef44f61b730b1ea72aa68b2d80996
sankaaaa <olexa2404@gmail.com>
2023-06-06 15:38:29 +0300

    Update State.java

--- src/gameStates/State.java
+++ src/gameStates/State.java
@@ -1,5 +1,6 @@
 package gameStates;
 import UI.MenuButton;
+import audio.AudioPlayer;
 import main.Game;
 import java.awt.event.MouseEvent;
 import main.Game;
@@ -13,5 +14,12 @@ public class State {
     }
     public Game getGame(){
         return game;
+    }
+    public void setGameState(GameState state){
+        switch(state){
+            case MENU -> game.getAudioPlayer().playSong(AudioPlayer.MENU_1);
+            case PLAYING -> game.getAudioPlayer().setLevelSong(AudioPlayer.LEVEL_2);
+        }
+        GameState.state = state;
     }
 }
